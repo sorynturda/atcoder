a647eadf57471c6d64a97d9d671a2334bf8efb10
Sorin Turda <s.turda12@gmail.com>
2023-11-23 10:33:57 +0200

    arbore partial de cost minim

---
+++ 328/e.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void MST(){
+
+}
+
+int main() {
+    int n,m,k;
+    // union sau kruskal, cu coada de prioritate
+    // caz particular n maxim 8 se cauta minimul din toate posibilitatile  
+    return 0;
+}
